e496e352e2cc482cfefeb0a30638c51096e60d0c
Ekaterina Kizikova <kizikovaek@yandex.ru>
2023-02-02 14:07:41 +0700

    Сделать проектную работу 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,15 @@
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__button-close');
+
+const popup = document.querySelector('.popup');
+const popupFormElement = document.querySelector('.popup__form');
+const popupFormNameInput = document.querySelector('.popup__input_type_name');
+const popupFormJobInput = document.querySelector('.popup__input_type_profession');
+
+editButton.addEventListener("click", handleEditButtonClick);
+closeButton.addEventListener("click", handleCloseButtonClick);
+popup.addEventListener("click", handleOverlayClick);
+
 
 function toggleOpenPopup() {
     popup.classList.toggle("popup_opened");
@@ -20,6 +29,32 @@ function handleOverlayClick(event) {
     }
 }
 
-editButton.addEventListener("click", handleEditButtonClick);
-closeButton.addEventListener("click", handleCloseButtonClick);
-popup.addEventListener("click", handleOverlayClick);
+function isProfileInfoValid (jobName, profileName) {
+    // if (jobName === "") return false;
+    // if (profileName === "") return false;
+    if (jobName.trim() === "") return false;
+    if (profileName.trim() === "") return false;
+
+    return true;
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+
+    const jobName = popupFormJobInput.value;
+    const profileName = popupFormNameInput.value;
+
+    if (!isProfileInfoValid(jobName, profileName)) return;
+
+    const profileNamePlaceholder = document.querySelector('.profile__name');
+    const jobNamePlaceholder = document.querySelector('.profile__text');
+
+    profileNamePlaceholder.textContent = profileName;
+    jobNamePlaceholder.textContent = jobName;
+
+    toggleOpenPopup();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+popupFormElement.addEventListener('submit', handleFormSubmit);
