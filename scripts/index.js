92f3cf08871e023262c1dc43b2622b06564ac5d5
Ekaterina Kizikova <kizikovaek@yandex.ru>
2023-02-08 15:32:38 +0700

    Внести правки

--- scripts/index.js
+++ scripts/index.js
@@ -6,33 +6,12 @@ const popupFormElement = document.querySelector('.popup__form');
 const popupFormNameInput = document.querySelector('.popup__input_type_name');
 const popupFormJobInput = document.querySelector('.popup__input_type_profession');
 
-const profileNamePlaceholder = document.querySelector('.profile__name');
-const jobNamePlaceholder = document.querySelector('.profile__text');
-
-
-// function toggleOpenPopup() {
-//     if (popup.classList.toggle("popup_opened")) {
-//         popupFormJobInput.value = jobNamePlaceholder.textContent;
-//         popupFormNameInput.value = profileNamePlaceholder.textContent;
-//     }
-
-// popupFormJobInput.value = jobNamePlaceholder.textContent
-// popupFormNameInput.value = profileNamePlaceholder.textContent
-
-// popup.classList.toggle("popup_opened");
-// }
-
-// function handleEditButtonClick() {
-//     toggleOpenPopup();
-// }
-
-// function handleCloseButtonClick() {
-//     toggleOpenPopup();
-// }
+const profileNameElement = document.querySelector('.profile__name');
+const jobNameElement = document.querySelector('.profile__text');
 
 function handleEditButtonClick() {
-    popupFormJobInput.value = jobNamePlaceholder.textContent;
-    popupFormNameInput.value = profileNamePlaceholder.textContent;
+    popupFormJobInput.value = jobNameElement.textContent;
+    popupFormNameInput.value = profileNameElement.textContent;
 
     popup.classList.add("popup_opened");
 }
@@ -52,8 +31,6 @@ function handleOverlayClick(event) {
 }
 
 function isProfileInfoValid(jobName, profileName) {
-    // if (jobName === "") return false;
-    // if (profileName === "") return false;
     if (jobName.trim() === "") return false;
     if (profileName.trim() === "") return false;
 
@@ -68,14 +45,12 @@ function handleFormSubmit(evt) {
 
     if (!isProfileInfoValid(jobName, profileName)) return;
 
-    profileNamePlaceholder.textContent = profileName;
-    jobNamePlaceholder.textContent = jobName;
+    profileNameElement.textContent = profileName;
+    jobNameElement.textContent = jobName;
 
     closePopup();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 editButton.addEventListener("click", handleEditButtonClick);
 closeButton.addEventListener("click", handleCloseButtonClick);
 popup.addEventListener("click", handleOverlayClick);
